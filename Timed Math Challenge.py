7fb050ae1f6540e996ab248bff0512931b01defe
Harrison Hank <codinghank@gmail.com>
2023-11-06 11:23:04 +0300

    modified:   Timed Math Challenge.py

--- Timed Math Challenge.py
+++ Timed Math Challenge.py
@@ -12,12 +12,12 @@ def generate_problem():
     operator = random.choice(OPERATORS)
 
     expr = str (left) + ' ' + operator + ' ' + str(right)
-    print (expr)
+    # print (expr)
     answer = eval(expr)
     return expr, answer
 
     expr, answer = generate_problem()
-    print(expr, answer)
+    # print(expr, answer)
 
 for i in range(TOTAL_PROBLEMS):
     expr, answer = generate_problem()
@@ -26,7 +26,7 @@ for i in range(TOTAL_PROBLEMS):
         if guess == str(answer):
             print("Correct!")
             break
-    print(expr, answer)
+    # print(expr, answer)
     # user_answer = int(input('What is the answer? '))
     # if user_answer == answer:
     #     print('Correct!')
