2d931694f452c527b3588c5df7ad4a5ce4fddeaf
Harrison Hank <codinghank@gmail.com>
2023-11-06 11:27:22 +0300

    modified:   Timed Math Challenge.py

--- Timed Math Challenge.py
+++ Timed Math Challenge.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 OPERATORS = ['+', '-', '*', ] # '/'
 MIN_OPERAND = 2
@@ -19,6 +20,11 @@ def generate_problem():
     expr, answer = generate_problem()
     # print(expr, answer)
 
+wrong = 0
+input('Press enter to start: ')
+print('Starting in...')
+start_time = time.time()  # get the current time
+
 for i in range(TOTAL_PROBLEMS):
     expr, answer = generate_problem()
     while True:
@@ -26,6 +32,12 @@ for i in range(TOTAL_PROBLEMS):
         if guess == str(answer):
             print("Correct!")
             break
+        wrong += 1
+
+end_time = time.time()
+total_time = end_time - start_time
+
+print("You got " + str(TOTAL_PROBLEMS - wrong) + " problems correct in " + str(total_time) + " seconds.")
     # print(expr, answer)
     # user_answer = int(input('What is the answer? '))
     # if user_answer == answer:
